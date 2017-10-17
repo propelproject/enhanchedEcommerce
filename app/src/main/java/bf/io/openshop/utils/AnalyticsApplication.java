eeabcc8c762a9bf438be2763cac9dcfc0f961ee2
reivinoktavianus <reivin.oktavianus@gmail.com>
2017-10-17 12:53:47 +0700

    Initial commit

---
+++ app/src/main/java/bf/io/openshop/utils/AnalyticsApplication.java
@@ -0,0 +1,38 @@
+package bf.io.openshop.utils;
+
+import android.app.Application;
+
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
+import bf.io.openshop.R;
+
+/**
+ * Created by asus on 10/17/2017.
+ */
+
+public class AnalyticsApplication extends Application {
+
+    private static GoogleAnalytics sAnalytics;
+    private static Tracker sTracker;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        sAnalytics = GoogleAnalytics.getInstance(this);
+    }
+
+    /**
+     * Gets the default {@link Tracker} for this {@link Application}.
+     * @return tracker
+     */
+    synchronized public Tracker getDefaultTracker() {
+        // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
+        if (sTracker == null) {
+            sTracker = sAnalytics.newTracker(R.xml.global_tracker);
+        }
+
+        return sTracker;
+    }
+}
